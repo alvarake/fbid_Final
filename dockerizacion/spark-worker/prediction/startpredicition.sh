d00b98e59d4b4d4e171bab63abd883e0938416dd
Alvarake <alvarocepeda2@gmail.com>
2019-12-01 23:31:35 +0100

    Se ha usado imagen propia para los workers

---
+++ dockerizacion/spark-worker/prediction/startpredicition.sh
@@ -0,0 +1,1 @@
+/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1 --class es.upm.dit.ging.predictor.MakePrediction --master spark://spark-master:7077 /prediction-job/flight_prediction_2.11-0.1.jar
